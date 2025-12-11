c11dba181373820a11dc8c57d8ba25c4ccba8861
Sergey Kokunov <sergress@gmail.com>
2025-12-11 18:53:48 +0300

    Release 1.40.2

---
+++ Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = Version("1.40.2")
+
+let gitUrl = "https://raw.githubusercontent.com/SumSubstance/IdensicMobileSDK-iOS-Release/master/\(version)"
+
+enum checksums {
+    static let fisherman = "4223162cd483de00e5ef75acbd56e08846d45741075a5c7c225007a5ce63b3bc"
+}
+
+let package = Package(
+    name: "IdensicMobileSDK_Fisherman",
+    platforms: [
+        .iOS("13.0")
+    ],
+    products: [
+        .library(
+            name: "IdensicMobileSDK_Fisherman",
+            targets: [
+                "FishermanWrapper"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SumSubstance/IdensicMobileSDK-iOS.git", .exact(version)),
+        .package(url: "https://github.com/fingerprintjs/fingerprintjs-pro-ios", "2.11.0" ..< "3.0.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "IdensicMobileSDK_Fisherman",
+            url: "\(gitUrl)/IdensicMobileSDK_Fisherman-\(version).zip",
+            checksum: checksums.fisherman
+        ),
+        .target(
+            name: "FishermanWrapper",
+            dependencies: [
+                .product(name: "IdensicMobileSDK", package: "IdensicMobileSDK-iOS"),
+                "IdensicMobileSDK_Fisherman",
+                .product(name: "FingerprintPro", package: "fingerprintjs-pro-ios")
+            ],
+            path: "FishermanWrapper"
+        )
+    ]
+)
